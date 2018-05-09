6efc4c8620a31e1a6189eef5c1247bbb84afcce4
Jon Redeker <jon.redeker@gmail.com>
2018-05-09 11:02:41 -0400

    add custom stylesheet

--- main.js
+++ main.js
@@ -7,20 +7,29 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
+// our custom css
+const css = require('style.css')
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 400, height: 600})
+
+  // load the ourgroceries site
+  mainWindow.loadURL('https://www.ourgroceries.com/your-lists/')
+  mainWindow.webContents.on('did-finish-load', function(){
+    mainWindow.webContents.insertCSS()
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  // mainWindow.loadURL(url.format({
+  //   pathname: path.join(__dirname, 'index.html'),
+  //   protocol: 'file:',
+  //   slashes: true
+  // }))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
